5fd0a3d891b902417692fb9e5dcdeb1f104b2cb5
Jiajie Chen <c@jia.je>
2022-10-10 21:26:45 +0800

    Add debian bookworm

--- qemu.sh
+++ qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 -smp 4 -m 8192 -bios /usr/share/OVMF/OVMF_CODE.fd -boot n -net nic -net tap,ifname=minipxe-test,script=setup-tap.sh,downscript=no -serial mon:stdio -nographic
+qemu-system-x86_64 -smp 4 -m 8192 -bios /usr/share/OVMF/OVMF_CODE.fd -boot n -net nic -net tap,ifname=minipxe-test,script=setup-tap.sh,downscript=no -serial mon:stdio
